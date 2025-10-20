556f5c11a1700938443c126ba86c6c93f1bfc33a
Liceaga Abel <GF2DV4M@seat.vwgroup.com>
2025-10-20 14:44:10 +0200

    Añado comentario en fila 45 sobre días válidos

--- Bikehsare.py
+++ Bikehsare.py
@@ -42,6 +42,7 @@ def get_filters():
             print("Invalid input. Please enter a valid month or 'all'.")
 
     # Valid days
+	"""Aquí sólo ponemos los días válidos"""
     days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday', 'all']
 
     # Get user input for day
